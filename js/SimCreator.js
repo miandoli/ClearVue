a5f2af2b376319b87aee6dabd85f344427633673
miandoli <miandoli8@gmail.com>
2018-11-10 15:08:59 -0500

    Added data files

--- js/SimCreator.js
+++ js/SimCreator.js
@@ -1,10 +1,26 @@
-function createSim(input) {
-    var jqxhr = $.ajax({
-        type: "POST",
-        url: "/python/sim.py",
-        async: false,
-        data: { param: input },
-    });
-    var simResults = jqxhr.responseText;
-    return 0;
+function createSim(param) {
+    data = grabData();
+    return param;
+}
+
+function grabData() {
+    file = "";
+    text = readTextFile(file);
+}
+
+function readTextFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+            }
+        }
+    }
+    rawFile.send(null);
+    return allText;
 }
--- js/python/sim.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-def callSim(param):
-    param = 5
-
-param = sys.param[1]
-callSim(param)
