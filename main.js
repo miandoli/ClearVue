fae7035275a9cbe64c61e3377fb6428d6c6e8e4b
MikeyBoss <michaelbosik@gmail.com>
2018-11-10 22:48:14 -0500

    started styling main page

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function startSim(){
     var cars = document.getElementById("carslider").value;
     var facts = document.getElementById("factoryslider").value;
     var farms = document.getElementById("farmslider").value;
-    createSim(cars/100, farms/100, facts/100);
+    createSim(cars/100.0, farms/100.0, facts/100.0);
 }
 
 function sliderChange(val, id) {
